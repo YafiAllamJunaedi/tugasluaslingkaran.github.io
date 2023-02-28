05c27a89d36ce41c49eb4f5394b9204d642718ff
YafiAllamJunaedi <118795197+YafiAllamJunaedi@users.noreply.github.com>
2023-02-27 20:58:37 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var phi;
+var jariJari;
+var total;
+
+function luasLingkaran(a, b) {
+    phi = 3.14;
+    jariJari = a **2
+    total = jariJari * phi;
+    return total;
+}
+
+alert(luasLingkaran(8))
